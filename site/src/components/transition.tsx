028949b86484062a9c2ce2073c2c4eb2dcd225fc
John <saygex@sex.com>
2025-03-21 18:57:00 +0100

    Mest Uzbeka

--- site/src/components/transition.tsx
+++ site/src/components/transition.tsx
@@ -2,29 +2,50 @@ import React, { useState, useEffect, ReactNode } from 'react';
 import { useLocation } from 'react-router-dom';
 
 const TvNoiseTransition: React.FC = () => {
-  const [isTransitioning, setIsTransitioning] = useState(true);
+  // const [isTransitioning, setIsTransitioning] = useState(true);
   const location = useLocation();
+  const [imageProgress, setImageProgress] = useState(100);
+  
 
-  // useEffect(() => {
-  //   setIsTransitioning(true);
-  //   const timer = setTimeout(() => {
-  //     setIsTransitioning(false);
-  //   }, 50);
-  // })
+
 
   useEffect(() => {
-    setIsTransitioning(true);
-    const timer = setTimeout(() => {
-      setIsTransitioning(false);
-    }, 200);
+    setImageProgress(100)
+    const timer = setTimeout(
+      () => {
+        const imageInterval = setInterval(() => {
+          setImageProgress((prev) => {
+            if (prev <=0) {
+              clearInterval(imageInterval);
+              return prev;
+            }
+            return prev -1;
+          });
+        }, 15);
+      },
+      500
+    );
+    
 
     return () => clearTimeout(timer); // Cleanup timeout on component unmount or location change
   }, [location]);
 
   return (
-    <div className={isTransitioning ? "tv-noise":""}>
+    <div style={ {
+      backgroundColor:"black",
+      width: "100%",
+      height: "100%",
+      position: "fixed",
+      zIndex:100000,
+      imageRendering: "pixelated",
+      clipPath: `inset(${100 - imageProgress}% 0 0 0)`,
+      //opacity: imageProgress ==0 ? 1 : 0,
+    }
+    }
+      >
     </div>
   );
 };
+
 
 export default TvNoiseTransition;
